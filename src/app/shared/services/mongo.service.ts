aaa119f90448d993ce7629ec3c858a4bcc26523b
shashwat <shashwatmishra803@gmail.com>
2021-08-24 23:37:48 +0530

    removed monog reference

--- src/app/shared/services/mongo.service.ts
+++ src/app/shared/services/mongo.service.ts
@@ -1,6 +1,5 @@
 import { Injectable } from '@angular/core';
-import * as mongoose from 'mongoose';
-import bodyParser from "body-parser";
+
 
 @Injectable({
   providedIn: 'root'
@@ -11,9 +10,6 @@ export class MongoService {
    // this.connectdb()
    }
   connectdb(){
-    mongoose.connect('mongodb+srv://pranav:Varanasi@07@imfapp.jpxdm.mongodb.net/myFirstDatabase?retryWrites=true&w=majority',{useNewUrlParser: true, useFindAndModify: false})
-    .then(() =>  console.log('connection successful'))
-    .catch((err) => console.error(err));
-
+   
   }
 }
